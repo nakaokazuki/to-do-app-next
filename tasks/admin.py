cd07c59e0b43503524f3dde0c62654aee0570ba0
nakaokazuki <kuroneko0804777@gmail.com>
2026-05-12 19:05:44 +0900

    6. モデルを作成する

--- tasks/admin.py
+++ tasks/admin.py
@@ -3,4 +3,8 @@ from django.contrib import admin
 from .models import Task
 
 
-admin.site.register(Task)
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("title", "scheduled_at", "location", "is_completed", "updated_at")
+    list_filter = ("is_completed",)
+    search_fields = ("title", "location", "detail")
--- tasks/models.py
+++ tasks/models.py
@@ -3,8 +3,14 @@ from django.db import models
 
 class Task(models.Model):
     title = models.CharField(max_length=100)
+    scheduled_at = models.DateTimeField(null=True, blank=True)
+    location = models.CharField(max_length=100, blank=True)
+    caution = models.TextField(blank=True)
+    preparation = models.TextField(blank=True)
+    detail = models.TextField(blank=True)
     is_completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
